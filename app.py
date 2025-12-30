7a35d08feb45e0e2c9f43381b9aa080d772d466d
navaneethchowdary38-lang <navaneethchowdary38@gmail.com>
2025-12-30 14:41:14 +0530

    Replace HuggingFaceEmbeddings import path

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ import streamlit as st
 from PyPDF2 import PdfReader
 
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
+
 from langchain_community.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains import RetrievalQA
